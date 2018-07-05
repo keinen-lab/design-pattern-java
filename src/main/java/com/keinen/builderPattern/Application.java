7f0f69a426e05d73310e04a9bf5abc3ecf85b386
keinen <rupang75@gmail.com>
2018-07-05 20:29:59 +0900

    add builer pattern
    add builder pattern

--- src/main/java/com/keinen/builderPattern/Application.java
+++ src/main/java/com/keinen/builderPattern/Application.java
@@ -1,6 +1,118 @@
 package com.keinen.builderPattern;
 
+// https://ko.wikipedia.org/wiki/%EB%B9%8C%EB%8D%94_%ED%8C%A8%ED%84%B4
+// Product
+class Pizza {
+    private String dough = "";
+    private String sauce = "";
+    private String topping = "";
+
+    public void setDough(String dough) {
+        this.dough = dough;
+    }
+
+    public void setSauce(String sauce) {
+        this.sauce = sauce;
+    }
+
+    public void setTopping(String topping) {
+        this.topping = topping;
+    }
+
+    @Override
+    public String toString() {
+        return "Pizza{" +
+                "dough='" + dough + '\'' +
+                ", sauce='" + sauce + '\'' +
+                ", topping='" + topping + '\'' +
+                '}';
+    }
+}
+
+// abstract builder
+abstract class PizzaBuilder {
+    protected Pizza pizza;
+
+    public Pizza getPizza() {
+        return pizza;
+    }
+
+    public void createNewPizzaProduct() {
+        pizza = new Pizza();
+    }
+
+    public abstract void buildDough();
+    public abstract void buildSauce();
+    public abstract void buildTopping();
+}
+
+// concrete Builder
+class HawaiianPizzaBuilder extends PizzaBuilder {
+    @Override
+    public void buildDough() {
+        pizza.setDough("cross");
+    }
+
+    @Override
+    public void buildSauce() {
+        pizza.setSauce("mild");
+    }
+
+    @Override
+    public void buildTopping() {
+        pizza.setTopping("ham+pineapple");
+    }
+}
+
+// concrete Builder
+class SpicyPizzaBuilder extends PizzaBuilder {
+    @Override
+    public void buildDough() {
+        pizza.setDough("pan baked");
+    }
+
+    @Override
+    public void buildSauce() {
+        pizza.setSauce("hot");
+    }
+
+    @Override
+    public void buildTopping() {
+        pizza.setTopping("pepperoni+salami");
+    }
+}
+
+// Director
+class Cook {
+    private PizzaBuilder pizzaBuilder;
+
+    public void setPizzaBuilder(PizzaBuilder pizzaBuilder) {
+        this.pizzaBuilder = pizzaBuilder;
+    }
+
+    public Pizza getPizza() {
+        return pizzaBuilder.getPizza();
+    }
+
+    public void constructPizza() {
+        pizzaBuilder.createNewPizzaProduct();
+        pizzaBuilder.buildDough();
+        pizzaBuilder.buildSauce();
+        pizzaBuilder.buildTopping();
+    }
+}
+
 public class Application {
     public static void main(String[] args) {
+        Cook cook = new Cook();
+        PizzaBuilder hawaiianPizzaBuilder = new HawaiianPizzaBuilder();
+        PizzaBuilder spicyPizzaBuilder = new SpicyPizzaBuilder();
+
+        cook.setPizzaBuilder(spicyPizzaBuilder);
+        cook.constructPizza();
+
+        Pizza pizza = cook.getPizza();
+
+        System.out.println(pizza.toString());
     }
 }
