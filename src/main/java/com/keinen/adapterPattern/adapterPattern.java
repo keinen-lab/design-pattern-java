11948ac238cc6995962bebbecdadeabbbc198fc4
keinen <rupang75@gmail.com>
2018-07-10 10:21:56 +0900

    add adapter pattern
    add adapter pattern

---
+++ src/main/java/com/keinen/adapterPattern/adapterPattern.java
@@ -0,0 +1,38 @@
+package com.keinen.adapterPattern;
+
+interface Stack<T> {
+    void push(T o);
+    T pop();
+    T top();
+}
+
+class DList<T> {
+    public void insert(DNode pos, T o) {}
+    public void remove(DNode pos) {}
+
+    public void insertHead(T o) {}
+    public void insertTail(T o) {}
+
+    public T removeHead() {}
+    public T removeTail() {}
+
+    public T getHead() {}
+    public T getTail() {}
+}
+
+class DListImpStack<T> extends DList<T> implements Stack<T> {
+    public void push(T o) {
+        insertTail(o);
+    }
+
+    public T pop() {
+        return removeTail();
+    }
+
+    public T top() {
+        return getTail();
+    }
+}
+
+public class adapterPattern {
+}
